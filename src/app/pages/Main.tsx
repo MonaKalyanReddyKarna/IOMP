ac383f37ef8114fee7f64fb6e60c8a4797b2aef2
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-08-24 03:24:55 +0530

    Initial Commit

---
+++ src/app/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// This is the main page of the app, write only calling for components here, main page will be in components folder
+
+const Main = () => {
+    return (
+        <div className='w-full h-screen'>
+        </div>
+    )
+}
+
+export default Main
