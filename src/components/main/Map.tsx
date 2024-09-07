65fc20bb7ca7e78de74920818299286bbcd70842
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-09-07 17:08:42 +0530

    added map

--- src/components/main/Map.tsx
+++ src/components/main/Map.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const Map = () => {
+mapboxgl.accessToken = 'pk.eyJ1IjoibXJmbHluIiwiYSI6ImNsd3YzOWswMDBhc3YyaXNheGc3aTRtdTcifQ.vqe0vVgE90a8B2CH9lYjUg';
+const MainMap = () => {
+    const mapContainer = useRef<any>(null);
+    const map = useRef<any>(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
   return (
-    <div>Map</div>
+      <div>
+          <div ref={mapContainer} className="map-container w-full h-full"/>
+      </div>
   )
 }
 
-export default Map
+export default MainMap;
