cbefb02c8668105f7126c53e41e32524c47043eb
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-09-08 02:32:58 +0530

    fixed styling

--- src/app/pages/Main.tsx
+++ src/app/pages/Main.tsx
@@ -5,10 +5,10 @@ import React from 'react'
 const Main = () => {
     return (
         <div className='w-full h-screen bg-gray-100 flex'>
-            <div className='w-1/2 bg-gray-100 border-r-2'>
+            <div className='w-full lg:w-1/2 bg-gray-100 border-r-2'>
                 <Hero />
             </div>
-            <div className='w-1/2 bg-gray-100'>
+            <div className='hidden lg:block w-1/2 bg-gray-100'>
                 <Map />
             </div>
         </div>
