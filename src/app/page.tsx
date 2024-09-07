0e98c0e28ee074303791ef355313edc0ce6924ad
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-09-07 15:00:53 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import { Charts } from "@/components/Charts";
-import { DisasterTable } from "@/components/DisasterTable";
+// import { Charts } from "@/components/Charts";
+// import { DisasterTable } from "@/components/DisasterTable";
 import { DUMMY } from "@/data/dummy";
 
 export default function Dashboard() {
   const initialDisasterData = DUMMY;
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      <main className="flex-1 flex p-8 overflow-auto flex-col gap-6">
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-3xl font-bold text-gray-800">Disaster Tracker</h2>
-        </div>
-
-        <Charts disasters={initialDisasterData} />
-        <DisasterTable disasters={initialDisasterData} />
-      </main>
-    </div>
+    <div></div>
   );
 }
