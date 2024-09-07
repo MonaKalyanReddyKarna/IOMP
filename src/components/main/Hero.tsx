586172df0bd2241055cf415a4d5ada5594d1e0a9
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-09-07 17:26:14 +0530

    updated

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -1,14 +1,18 @@
 "use client"
-import React, { useState } from 'react'
-import { data } from "@/data/dummy"
-import Disasters from './Disasters'
+import React, { useState } from 'react';
+import Disasters from './Disasters';
+import { useDisasterStore } from '@/zustand/useDisasterStore';
 
 const Hero = () => {
+    const { disasters } = useDisasterStore();
+    const [section, setSection] = useState<'disasters' | 'disasterinfo'>('disasters');
+
     return (
         <div className='p-2 h-full overflow-y-auto'>
-            <Disasters disasters={data} />
+            <Disasters disasters={disasters} />
+            {/* <DisasterInfo /> */}
         </div>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
