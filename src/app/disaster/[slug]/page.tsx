a326fb3fc9ccdd7249f18349ec5b2e6a81902288
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-09-09 09:20:53 +0530

    final

--- src/app/disaster/[slug]/page.tsx
+++ src/app/disaster/[slug]/page.tsx
@@ -12,7 +12,7 @@ import { Disaster } from '@/types/disaster';
 const Page = ({ params }: { params: { slug: string } }) => {
     const disaster: Disaster = data.find((disaster) => disaster._id == params.slug) as Disaster;
     return (
-        <div className='w-full h-screen'>
+        <div className='w-full max-w-[80vw] mx-auto h-screen'>
             <div className='w-full h-full flex flex-col justify-evenly'>
                 {/* Heading */}
                 <DisasterHeader key={params.slug} disasterData={disaster} />
