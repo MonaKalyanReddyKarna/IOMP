0560697b49667e0f772d67c2133b4d05eb8e13f2
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-09-09 09:57:25 +0530

    added header in config page

--- src/app/configurations/page.tsx
+++ src/app/configurations/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { configurations } from '@/data/configurations'
 import ConfigBox from './ConfigBox'
+import {ArrowDownWideNarrow, Filter} from "lucide-react";
 
 const Configurations = () => {
 
     return (
-        <div className='flex flex-wrap items-center justify-center px-8 py-4 gap-10'>
-            {configurations.map((configuration, index) => (
-                <ConfigBox key={index} configuration={configuration} />
-            ))}
+        <div>
+            <div className='flex flex-row justify-between w-full p-4 rounded-xl '>
+                <h1 className='text-5xl text-black font-[700] pl-4'>Scrapper Configurations</h1>
+            </div>
+            <div className='flex flex-wrap items-center justify-center px-8 py-4 gap-10'>
+                {configurations.map((configuration, index) => (
+                    <ConfigBox key={index} configuration={configuration}/>
+                ))}
+            </div>
         </div>
     )
 }
